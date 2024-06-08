7b3679a323478d11820e64017b39e7c416b75308
Morey <orantarine@gmail.com>
2024-06-08 12:16:38 +0200

    Add git

---
+++ src/main/java/org/morey/brumeterre/main.java
@@ -0,0 +1,24 @@
+package org.morey.brumeterre;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class main extends JavaPlugin {
+
+    public Logger log = getLogger();
+
+    @Override
+    public void onEnable() {
+
+        log.info("Brumeterre started.");
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        log.info("Brumeterre stopped.");
+
+    }
+}
